a2f98a9cf8a1ba612dbac7b4055641d51e4cc526
xieke <xieke@rd-xieke.litb-inc.com>
2016-01-14 17:27:27 +0800

    finished problem-110

--- leetcode/src/main/java/leetcode/BalancedBinaryTree110.java
+++ leetcode/src/main/java/leetcode/BalancedBinaryTree110.java
@@ -11,18 +11,53 @@ public class BalancedBinaryTree110 {
 	 * in which the depth of the two subtrees 
 	 * of every node never differ by more than 1.
 	 */
-
-
-
-
-
-
+	
+	TreeNode root;
 	public boolean isBalanced(TreeNode root) {
-
+		if (root == null)
+			return true;
+		else if (Math.abs(height(root.left) - height(root.right)) <= 1){
+			if (isBalanced(root.left) && isBalanced(root.right))
+				return true;
+		} 
 		return false;
 	}
-	
-	
+
+	public int height(TreeNode node){
+		if (node == null)
+			return 0;
+		else {
+			return 1 + Math.max(height(node.left), height(node.right));
+		}
+	}
+
+	public void insert(int val){
+		TreeNode newTreeNode = new TreeNode(val);
+		if (root == null)
+			root = new TreeNode(val);
+		else {
+			TreeNode current = root;
+			TreeNode parent;
+			while(true){
+				parent = current;
+				if (val < parent.val){
+					current = current.left; 
+					if (current == null){
+						parent.left = newTreeNode; 
+						return ;
+					}
+				} else {
+					current = current.right;  
+					if (current == null){
+						parent.right = newTreeNode;
+						return ;
+					}
+				}
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
 		TreeNode node1 = new TreeNode(10);
 		TreeNode node2 = new TreeNode(20);
