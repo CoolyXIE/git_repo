ac82b726093114576b4f6d0e691c26d195e6c8c1
xieke <xieke@rd-xieke.litb-inc.com>
2016-01-26 10:31:45 +0800

    add python and leetcode

---
+++ firstPython/100/8.py
@@ -0,0 +1,8 @@
+for i in range(1, 10):
+    for j in range(1, 10):
+        if (j == 9):
+            print '%d*%d=%-2d\t'%(i,j,i*j)
+        else:
+            print '%d*%d=%-2d\t'%(i,j,i*j),
+        
+
